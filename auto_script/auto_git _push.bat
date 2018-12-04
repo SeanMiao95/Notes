ddcc76366a6adf056434cbf79c1e12f1ac868d70
Sean <1319587163@qq.com>
2018-12-04 17:48:13 +0800

    Sean add something new

---
+++ auto_script/auto_git _push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\ff\Desktop\Notes
+git add .
+git commit -m "Sean add something new"
+git push origin master
---
+++ auto_script/auto_git_pull.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\ff\Desktop\Notes
+git pull origin master
